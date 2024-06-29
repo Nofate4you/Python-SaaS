91aa10b77c6ef2cf585428b4eb550b82c2daafbc
Your Name <ke.bleach@gmail.com>
2024-06-28 20:51:55 -0500

    first commit

---
+++ src/cfehome/views.py
@@ -0,0 +1,15 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_template = "home.html"
+    return render(request, html_template, my_context)
+
+
