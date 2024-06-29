66975d090fd9182ad5764ec62e058789be48c7b3
Your Name <ke.bleach@gmail.com>
2024-06-29 16:05:05 -0500

    Hello world msg

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my-apps
+    "visits",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'cfehome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "src/cfehome/templates"],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                  
             ],
         },
     },
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from .views import home_page_view
 
-
 urlpatterns = [
-    path("", home_page_view), #index page ->root page
-    path("hello-world/", home_page_view),
-    path("admin/", admin.site.urls),
+    path('', home_page_view),
+    path('hello-world/', home_page_view),
+    path('admin/', admin.site.urls),
 ]
